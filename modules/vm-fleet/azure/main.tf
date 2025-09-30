76eb0996b4da884d58a4d7a5f43c34641305674b
jayonthenet <clemens@humanitec.com>
2025-09-30 13:33:07 +0200

    fix: update SKU for Ubuntu server image to 22_04-lts
    Signed-off-by: jayonthenet <clemens@humanitec.com>

--- modules/vm-fleet/azure/main.tf
+++ modules/vm-fleet/azure/main.tf
@@ -188,7 +188,7 @@ resource "azurerm_linux_virtual_machine" "fleet" {
   source_image_reference {
     publisher = "Canonical"
     offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts-gen2"
+    sku       = "22_04-lts"
     version   = "latest"
   }
 }
