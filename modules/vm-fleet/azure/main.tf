a14cb32d48a233139b410d5dfe6e80f9df74268f
jayonthenet <clemens@humanitec.com>
2025-09-30 12:55:12 +0200

    fix: update resource group location to westeurope
    Signed-off-by: jayonthenet <clemens@humanitec.com>

--- modules/vm-fleet/azure/main.tf
+++ modules/vm-fleet/azure/main.tf
@@ -21,7 +21,7 @@ data "azurerm_client_config" "current" {}
 # Create a resource group for the fleet
 resource "azurerm_resource_group" "fleet" {
   name     = "vm-fleet-${random_id.fleet_id.hex}"
-  location = "East US"
+  location = "westeurope"
 }
 
 # Create virtual network
